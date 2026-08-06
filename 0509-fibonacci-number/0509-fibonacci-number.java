5de1a9abf597d5af99a506990593dd91c35e622b
PRIYANSHU SINGH <priyanshusingh192004@gmail.com>
2026-08-06 08:25:37 +0530

    Time: 0 ms (100%), Space: 42 MB (53.14%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,14 +1,11 @@
 class Solution {
-    public int fibo(int n,int[] dp){
-        if(n<=1) return n;
-        if(dp[n]!=0) return dp[n];
-        int ans = fibo(n-1,dp) + fibo(n-2,dp);
-        dp[n] = ans;
-        return ans;
-          
-    }
     public int fib(int n) {
-        int[] dp = new int[n+1]; // index from 0 to n;
-        return fibo(n,dp);
+        if(n<=1) return n;
+        int[] dp = new int[n+1];
+        dp[0] = 0; dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
