b7e80e176ff2600180af8543c1ecb8504c337db0
PRIYANSHU SINGH <priyanshusingh192004@gmail.com>
2026-08-06 07:54:38 +0530

    Time: 0 ms (100%), Space: 42.1 MB (34.01%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    static int[] dp;
+    public int fibo(int n){
+        if(n<=1) return n;
+        if(dp[n]!=0) return dp[n];
+        int ans = fibo(n-1) + fibo(n-2);
+        dp[n] = ans;
+        return ans;
+          
+    }
+    public int fib(int n) {
+        dp = new int[n+1]; // index from 0 to n;
+        return fibo(n);
+    }
+}
