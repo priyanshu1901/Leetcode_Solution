0c53e06c9369ffeba84b2b8108e94acddb49ce42
PRIYANSHU SINGH <priyanshusingh192004@gmail.com>
2026-08-08 13:29:01 +0530

    Time: 0 ms (100%), Space: 42 MB (83.35%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -4,11 +4,8 @@ class Solution {
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(i==0 || j==0){
-                    dp[0][j] = 1;
-                    dp[i][0] = 1;
-                }
-                if(j>0 && i>0) dp[i][j] = dp[i][j-1] + dp[i-1][j];
+                if(i==0 || j==0) dp[i][j] = 1;
+                else dp[i][j] = dp[i][j-1] + dp[i-1][j];
             }
         }
         return dp[m-1][n-1];
